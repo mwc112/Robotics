83ba8fc75f15a41731dbee883222b84afe9fefed
pi <pi@raspberrypi.(none)>
2016-02-02 15:28:30 +0000

    THINGS

--- motor_calibration.py
+++ motor_calibration.py
@@ -5,6 +5,8 @@ def sub_misc(x, tup):
 
 import brickpi
 import time
+from os import listdir
+from os.path import isfile
 
 interface=brickpi.Interface()
 interface.initialize()
@@ -31,8 +33,8 @@ lparams.pidParameters.minOutput = -255
 lparams.pidParameters.maxOutput = 255
 
 
-lp = 600
-rp = 600
+lp = 810
+rp = 770
 
 
 lparams.pidParameters.k_i = 0
@@ -43,24 +45,27 @@ rparams.pidParameters.k_i = 0
 rparams.pidParameters.k_d = 0
 rparams.pidParameters.k_p = rp
 
-angle = -20
+angle = -55
 interface.setMotorAngleControllerParameters(motors[0],lparams)
 interface.setMotorAngleControllerParameters(motors[1],rparams)
 
 
-for i in range(1,20):
-    interface.startLogging("logs/k_p{0}-{1}.txt".format(lp,rp))
-    interface.increaseMotorAngleReferences(motors,[angle,angle])
-    targetAngles = interface.getMotorAngleReferences(motors)
-    lp += 20
-    rp += 20
+for i in range(1,12):
+    if (not isfile("logs/k_p{0}-{1}.txt".format(lp,rp))):
+         interface.startLogging("logs/k_p{0}-{1}.txt".format(lp,rp))
+         interface.increaseMotorAngleReferences(motors,[angle,angle])
+         targetAngles = interface.getMotorAngleReferences(motors)
+         time.sleep(3.5)
+         interface.stopLogging()
+         raw_input("Done lp:{0} rp:{1}".format(lp,rp))
+    else:
+         print "skipping {0},{1}".format(lp,rp)
+    lp += 2
+    rp += 2
     lparams.pidParameters.k_p = lp
     rparams.pidParameters.k_p = rp
     interface.setMotorAngleControllerParameters(motors[0], lparams)
     interface.setMotorAngleControllerParameters(motors[1], rparams)
-    time.sleep(3.5)
-    interface.stopLogging()
-    raw_input("Done lp:{0} rp:{1}".format(lp,rp))
 
 interface.terminate()
 
