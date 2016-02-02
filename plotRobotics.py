ae2bbb871e84306b9aa7bf452ba86e911b8a7173
Christopher Love <cjl113@voxel15.doc.ic.ac.uk>
2016-02-02 14:37:47 +0000

    SCRIPT

--- plotRobotics.py
+++ plotRobotics.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
-import matplotlib.pyplot as plt
 import csv
 
+from scipy.optimize import curve_fit
 from os import listdir
 from os.path import isfile, join
+import matplotlib.pyplot as plt
+import numpy as np
 
-path = "logsSleep/"
+# create the function we want to fit
+def my_sin(x, freq, amplitude, phase, offset):
+        return np.sin(x * freq + phase) * amplitude + offset
+
+path = "../logsF2/"
 
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
@@ -44,27 +50,51 @@ try:
                     break;
             count +=1
         tsv.close()
+        
+#        N = count + 1 # number of data points
+#        t = np.linspace(0, 15*np.pi, N)
+#        guess_freq = 1
+#        guess_amplitude = 10*np.std(leftErr)/(2**0.5)
+#        guess_phase = 0
+#        guess_offset = np.mean(leftErr)        
+#     
+#        p0=[guess_freq, guess_amplitude,
+#                    guess_phase, guess_offset]
+#     
+#        # now do the fit
+#        fit = curve_fit(my_sin, t, leftErr, p0=p0)
+#        data_fit = my_sin(t, *fit[0])
+
+        plt.clf()
         plt.plot(time, leftRef, 'b',  label="left reference")
         plt.plot(time, leftReal, 'k', label="left real")    
         plt.plot(time, leftErr, 'k', label="left error")    
-        plt.axis([0, time[count-1], -5, 21])    
+        plt.axis([time[0], time[count-1], -5, 21])    
         plt.axhline(0, color='black')
+        plt.axhline(min(leftErr), color='green')
+        plt.axhline(max(leftErr), color='green')
+        plt.axhline(np.mean(leftErr), color='green')
         plt.title(join("left ",log))
         plt.legend(loc=7)
         mng = plt.get_current_fig_manager()
         mng.resize(*mng.window.maxsize())
-        plt.show()
+        
+        plt.savefig(join(path,"log","left" + log[:-4] + ".png"))
+        plt.clf()
         plt.plot(time, rightRef, 'g',  label="right reference")
         plt.plot(time, rightReal, 'c', label="right real")     
         plt.plot(time, rightErr, 'k', label="right error")    
-        plt.axis([0, time[count-1], -5, 21])    
+        plt.axis([time[0], time[count-1], -5, 21])    
         plt.axhline(0, color='black')
         plt.title(join("right ",log))
         plt.legend(loc=7)
+        plt.axhline(min(rightErr), color='green')
+        plt.axhline(max(rightErr), color='green')
+        plt.axhline(np.mean(rightErr), color='green')
         mng = plt.get_current_fig_manager()
         mng.resize(*mng.window.maxsize())
-        plt.show()
+        plt.savefig(join(path,"log","right" + log[:-4] + ".png"))
 except IOError:
-    print "lol"
+    print "IO"
 
 
